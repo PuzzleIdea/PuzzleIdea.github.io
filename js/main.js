abdb763a2cbae53a7e5a214c19b9b960aab40401
PuzzleIdea <weizishen400@gmail.com>
2026-04-09 20:11:26 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -395,7 +395,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     async function fetchBilibiliViews() {
-        const els = document.querySelectorAll('.game-views[data-bvid]');
+        const els = document.querySelectorAll('.experimentalgamesgame-views[data-bvid]');
         if (!els.length) return;
         const bvidMap = {};
         els.forEach(el => {
